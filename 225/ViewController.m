3142813f2c1dc0a60149524a05e9cbcdf489bfdc
Julie0e <julie_91@hanmail.net>
2014-01-07 18:21:18 +0900

    225 완료합니다.

--- 225/ViewController.m
+++ 225/ViewController.m
@@ -9,10 +9,62 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+{
+    BOOL moveFlag;
+}
+@property (weak, nonatomic)IBOutlet UIImageView *imageView;
 @end
 
 @implementation ViewController
+
+- (IBAction)series:(id)sender
+{
+    [UIView beginAnimations:@"series1" context:NULL];
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDuration:1.0];
+    // 델리게이트
+    [UIView setAnimationDelegate:self];
+    [UIView setAnimationDidStopSelector:@selector(changeBackgroundClear)];
+    
+    self.imageView.backgroundColor = [UIColor blackColor];
+    
+    [UIView commitAnimations];
+    
+    // 블록을 이용한 코드
+}
+
+- (void)changeBackgroundClear
+{
+    [UIView beginAnimations:@"series2" context:NULL];
+    [UIView setAnimationDuration:1.0];
+    [UIView setAnimationDelegate:self];
+    
+    self.imageView.backgroundColor = [UIColor clearColor];
+    
+    [UIView commitAnimations];
+}
+
+- (IBAction)move:(id)sender
+{
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:0.7];
+    
+    if (moveFlag) {
+        self.imageView.center = CGPointMake(self.imageView.center.x+100, self.imageView.center.y+100);
+    }
+    else
+    {
+        self.imageView.center = CGPointMake(self.imageView.center.x-100, self.imageView.center.y-100);
+    }
+    moveFlag =! moveFlag;
+    [UIView commitAnimations];
+}
+
+-(IBAction)alpha:(id)sender
+{
+    [UIView animateWithDuration:0.7 animations:^{
+        self.imageView.alpha = (self.imageView.alpha == 0.0)? 1.0 : 0.0;}];
+}
 
 - (void)viewDidLoad
 {
